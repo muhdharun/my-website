13ee02593871b904c0f21bc6bad0de7fc62a7c3a
muhdharun <harunabdul@yahoo.com.sg>
2020-08-16 18:49:14 +0800

    Updates

---
+++ precache-manifest.835a205a2b6b5e222a90a60a17e04aa5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d62ac2216b6f9edf7334a7f4409a6e",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "df42ffe6799e0405dfae",
+    "url": "/my-website/static/css/main.0051df16.chunk.css"
+  },
+  {
+    "revision": "55a5ab70d917c0ba1e5c",
+    "url": "/my-website/static/js/2.629bb1a6.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/my-website/static/js/2.629bb1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df42ffe6799e0405dfae",
+    "url": "/my-website/static/js/main.b82cdfc0.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  },
+  {
+    "revision": "436b642e286021aabd91872e36fb3ad0",
+    "url": "/my-website/static/media/4xlabs.436b642e.jpg"
+  },
+  {
+    "revision": "2e8023290b91cae7a431c444a148fb02",
+    "url": "/my-website/static/media/cauldron.2e802329.jpg"
+  },
+  {
+    "revision": "1006ddc51e525b64927399124d87da88",
+    "url": "/my-website/static/media/comBackground3.1006ddc5.jpg"
+  },
+  {
+    "revision": "a84b4df72c68da27b0e414b94b61d22c",
+    "url": "/my-website/static/media/ml.a84b4df7.jpg"
+  },
+  {
+    "revision": "8324cdc8cd8befb79ece3261d31c3930",
+    "url": "/my-website/static/media/noc.8324cdc8.jpg"
+  },
+  {
+    "revision": "80b8afc06140d2d7b143c9cb7d515063",
+    "url": "/my-website/static/media/prism.80b8afc0.png"
+  },
+  {
+    "revision": "ced77093d109ab3bc2eeade1a8299ad9",
+    "url": "/my-website/static/media/profile.ced77093.png"
+  },
+  {
+    "revision": "c911854d67e1220a4d7d0d996a64b844",
+    "url": "/my-website/static/media/profile2.c911854d.png"
+  },
+  {
+    "revision": "4a6c1fb6c04f5445b290edf491249b99",
+    "url": "/my-website/static/media/schneider.4a6c1fb6.jpg"
+  },
+  {
+    "revision": "c71eea835341d1fa5bea78e76edb5699",
+    "url": "/my-website/static/media/vincent.c71eea83.jpg"
+  }
+]);
